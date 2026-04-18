f8abb101c1d4a6448b9a812e208bea13c342904b
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2026-04-17 20:45:46 -0600

    update: lexer class declaration

--- include/Lexer.hpp
+++ include/Lexer.hpp
@@ -1,34 +1,17 @@
 #ifndef LEXER_HPP
 #define LEXER_HPP
 
-#include "Data/TransitionMatrix.hpp"
-#include "List.hpp"
 #include <string>
-#include <vector>
-
-struct LexicalErrors {
-  int line;
-  int column;
-  std::string message;
-  std::string lexeme;
-};
+enum class InputType { EOI = 0, SEMI, PLUS, TIMES, LP, RP, NUM_OR_ID };
 
 class Lexer {
-private:
-  int state;
-  int category;
-  int line;
-  int column;
-  std::string buffer;
-  List TokenList;
-  std::vector<LexicalErrors> errors;
-
-public:
+ public:
   Lexer();
-  lexer::Symbol classifyChar(char c);
-  void createTokenList(char c);
-  void printTokenList();
-  void printErrors();
+
+ private:
+  std::string buffer;
+  int column;
+  int row;
 };
 
 #endif
