34819cc3b46ff425975864e935e0060bf956adc6
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2026-04-17 22:45:00 -0600

    update: main

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,12 @@
 #include "Lexer.hpp"
-#include <fstream>
-#include <iostream>
 
 int main() {
 
-  std::ifstream archivo("../docs/input.txt");
-  if (!archivo.is_open()) {
-    std::cerr << "Error: no se pudo abrir el archivo\n";
-    return -1;
-  }
-
-  char c;
   Lexer lexer;
+  std::string filename = "../docs/input.txt";
 
-  while (archivo.get(c)) {
-    lexer.createTokenList(c);
-  }
+  lexer.analizeFile(filename);
+  std::vector<data> tokens = lexer.getTokenList();
 
-  lexer.printTokenList();
-  std::cout << std::endl;
-  lexer.printErrors();
-
-  archivo.close();
   return 0;
 }
