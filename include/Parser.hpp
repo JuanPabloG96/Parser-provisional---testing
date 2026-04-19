8dc230bfbe09033255e3a5c362ab5f095e210a11
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2026-04-19 04:27:59 -0600

    update: parser declaration

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -1,8 +1,23 @@
-#pragma one
+#pragma once
+
+#include "Lexer.hpp"
 
 class Parser {
- public:
-  Parser();
-
  private:
+  std::vector<data> tokenList;
+  int current;
+
+  bool match(InputType type);
+  void advance();
+
+  void statements();
+  void expression();
+  void expr_prime();
+  void term();
+  void term_prime();
+  void factor();
+
+ public:
+  Parser(std::vector<data> tokenList);
+  void parse();
 };
