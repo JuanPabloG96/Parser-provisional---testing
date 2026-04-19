c89ac133b3fb13d0f3c73582a53f8f00b67434d1
Juan Pablo Gama Olivares <juanpablo.gama.olivares@gmail.com>
2026-04-19 04:30:10 -0600

    update: main, change testing file

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include "Lexer.hpp"
+#include "Parser.hpp"
 
 int main() {
-
   Lexer lexer;
-  std::string filename = "../docs/input.txt";
+  std::string filename = "../docs/simpleInput.txt";
 
   lexer.analizeFile(filename);
   std::vector<data> tokens = lexer.getTokenList();
+
+  Parser parser(tokens);
+  parser.parse();
 
   return 0;
 }
